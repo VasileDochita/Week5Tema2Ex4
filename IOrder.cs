73ca4f78a1406d96f2b74be6b40a7cd62e5ec8cc
VasileDochita <54108503+VasileDochita@users.noreply.github.com>
2019-11-12 17:14:32 +0200

    Add files via upload

---
+++ IOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Tema2Ex4
+{
+    public interface IOrder
+    {
+        
+        string RefferenceNumber { get; set; }
+
+        string StoreName { get; set; }
+
+        IVehicle Vehicle { get; set; }
+
+        DateTime DeliveryDate { get; set; }
+
+        void CancelOrder();
+    }
+
+}
---
+++ IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Tema2Ex4
+{
+    public interface IVehicle
+    {
+        IProducer Producer { get; private set; }
+
+        string Name { get; private set; }
+    }
+}
